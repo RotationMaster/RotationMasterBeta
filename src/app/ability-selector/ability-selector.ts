d38f6404d1078004e7ebef73b852366efd64fe1a
ellam fox <ellammalle_1994@hotmail.com>
2025-08-08 09:23:49 +0100

    ability selector

--- src/app/ability-selector/ability-selector.ts
+++ src/app/ability-selector/ability-selector.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { Ability, AbilitySelection } from '../../models';
+import { SearchDropdown } from '../search-dropdown/search-dropdown';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'rm-ability-selector',
-  imports: [],
-  templateUrl: './ability-selector.html',
-  styleUrl: './ability-selector.scss'
+    selector: 'rm-ability-selector',
+    templateUrl: './ability-selector.html',
+    styleUrls: ['./ability-selector.scss'],
+    imports: [SearchDropdown, FormsModule],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AbilitySelector {
+  @Input() abilitySelection: AbilitySelection = new AbilitySelection();
+  @Input() abilities: Ability[] = [];
+  @Input() isFirst: boolean = false;
+  @Input() isOnly: boolean = false;
 
+  @Output() abilitySelectionChange = new EventEmitter<AbilitySelection>();
+  @Output() delete = new EventEmitter();
+  @Output() copy = new EventEmitter();
+
+  separators: string[] = ['→', '+', '/', 's', 'r', 'tc',
+                          '↵ →', '↵ +', '↵ /', '↵ s', '↵ r', '↵ tc',
+                          '↵',''];
+
+  onAbilityChange(event: Ability) {
+    this.abilitySelection.SelectedAbility = event;
+    this.abilitySelectionChange.emit(this.abilitySelection);
+  }
+
+  onSeparatorChange(event: string) {
+    this.abilitySelection.Separator = event;
+    this.abilitySelectionChange.emit(this.abilitySelection);
+  }
+
+  onNotesChange(event: string) {
+    this.abilitySelection.Notes = event;
+    this.abilitySelectionChange.emit(this.abilitySelection);
+  }
 }
