5ab21bfaaddd10335dfe6dca9dbd2a35735df46d
ellam fox <ellammalle_1994@hotmail.com>
2025-08-08 09:40:57 +0100

    settings

--- src/app/settings/settings.ts
+++ src/app/settings/settings.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RangeSettingConfig } from '../../models';
+import { RangeSetting } from '../range-setting/range-setting';
 
 @Component({
   selector: 'rm-settings',
-  imports: [],
+  imports: [RangeSetting],
   templateUrl: './settings.html',
   styleUrl: './settings.scss'
 })
 export class Settings {
+  @Input() version: string = '0.0.1';
+  @Input() settings: RangeSettingConfig[] = [];
 
+  @Output() updateSetting = new EventEmitter<{ name: string, value: any }>();
+  @Output() setOverlayPosition = new EventEmitter();
 }
